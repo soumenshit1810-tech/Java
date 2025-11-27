f98a1f0f832a4bc9d837c575c40a97a97d4be897
soumenshit1810-tech <soumenshit1810@gmail.com>
2025-11-27 23:41:48 +0530

    add

---
+++ ListNode.java
@@ -0,0 +1,20 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
